b3a8f30953d51aed576ac460653f044fddafce70
Thiago Spindola <thiago_spdvr@hotmail.com>
2018-10-05 23:08:37 -0300

    hops project

---
+++ Hops/Hops.php
@@ -0,0 +1,166 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: thiag
+ * Date: 05/10/2018
+ * Time: 22:39
+ */
+
+class Hops{
+
+    private $nome;
+    private $geraniol;
+    private $humulene;
+    private $farnese;
+    private $caryophylene;
+    private $linalool;
+    private $myrcene;
+    private $bpinene;
+    private $outros;
+
+    /**
+     * @return mixed
+     */
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    /**
+     * @param mixed $nome
+     */
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getGeraniol()
+    {
+        return $this->geraniol;
+    }
+
+    /**
+     * @param mixed $geraniol
+     */
+    public function setGeraniol($geraniol)
+    {
+        $this->geraniol = $geraniol;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getHumulene()
+    {
+        return $this->humulene;
+    }
+
+    /**
+     * @param mixed $humulene
+     */
+    public function setHumulene($humulene)
+    {
+        $this->humulene = $humulene;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFarnese()
+    {
+        return $this->farnese;
+    }
+
+    /**
+     * @param mixed $farnese
+     */
+    public function setFarnese($farnese)
+    {
+        $this->farnese = $farnese;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCaryophylene()
+    {
+        return $this->caryophylene;
+    }
+
+    /**
+     * @param mixed $caryophylene
+     */
+    public function setCaryophylene($caryophylene)
+    {
+        $this->caryophylene = $caryophylene;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLinalool()
+    {
+        return $this->linalool;
+    }
+
+    /**
+     * @param mixed $linalool
+     */
+    public function setLinalool($linalool)
+    {
+        $this->linalool = $linalool;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMyrcene()
+    {
+        return $this->myrcene;
+    }
+
+    /**
+     * @param mixed $myrcene
+     */
+    public function setMyrcene($myrcene)
+    {
+        $this->myrcene = $myrcene;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getBpinene()
+    {
+        return $this->bpinene;
+    }
+
+    /**
+     * @param mixed $bpinene
+     */
+    public function setBpinene($bpinene)
+    {
+        $this->bpinene = $bpinene;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getOutros()
+    {
+        return $this->outros;
+    }
+
+    /**
+     * @param mixed $outros
+     */
+    public function setOutros($outros)
+    {
+        $this->outros = $outros;
+    }
+
+
+}
